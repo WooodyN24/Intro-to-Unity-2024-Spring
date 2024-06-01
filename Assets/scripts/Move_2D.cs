a458646aaec8e78e7d23ac63b511e43dd608e2d1
WooodyN24 <166954588+WooodyN24@users.noreply.github.com>
2024-06-01 14:34:28 -0700

    is playable again, however camera is not working

---
+++ Assets/scripts/Move_2D.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_2D : MonoBehaviour
+
+
+{
+    public float acceleration = 1.0f;
+    public float jumpforce = 1.0f;
+
+    private Rigidbody2D rb = null;
+    private bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    bool isGrounded()
+    {
+        int layerMask = LayerMask.GetMask("Grounded");
+        Vector2 offset = new Vector2(0.0f, -0.5f);
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        return Physics2D.Raycast(position + offset, Vector2.down,
+            0.1f, layerMask);
+    }
+    
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+        }
+    }
+
+    //Update  is called  once per frame
+    void FixedUpdate()
+    {
+        float mass = rb.mass;
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 movement = new Vector2(-acceleration * mass, 0.0f);
+            rb.AddForce(movement, ForceMode2D.Force);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Vector2 movement = new Vector2(acceleration * mass, 0.0f);
+            rb.AddForce(movement, ForceMode2D.Force);
+
+        }
+
+        if (isJumping && isGrounded())
+        { 
+            isJumping = false;
+            rb.AddForce(new Vector2(0.0f, jumpforce), ForceMode2D.Impulse);
+        }
+    }
+    
+}
